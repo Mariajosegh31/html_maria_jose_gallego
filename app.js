3a8748021737fb45da754a15d5c50e66abb95fea
Mariajosegh31 <123569496+Mariajosegh31@users.noreply.github.com>
2023-02-06 12:05:45 -0500

    6/03/2023

---
+++ app.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMcontentloaded", () => {
+const cardsAdj = [ 
+  {
+    name:'Caballo',
+    img:'imagenes/Caballo.jpg'
+  },
+  
+
+{
+  name:'Cabra',
+  img:'imagenes/Cabra.jpg'
+},
+
+{
+  name:'Cerdo',
+  img:'imagenes/Cerdo.jpg'
+},
+
+
+
+{
+  name:'Conejo',
+  img:'imagenes/Conejo.jpg'
+},
+
+{
+  name:'Oveja',
+  img:'imagenes/Oveja.jpg'
+},
+
+{
+  name:'Pavo',
+  img:'imagenes/Pavo.jpg'
+},
