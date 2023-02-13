8ac32be009d808b5b8b96b22fb597783e7ad8e04
Mariajosegh31 <123569496+Mariajosegh31@users.noreply.github.com>
2023-02-13 11:38:08 -0500

    13/02/2023

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
 document.addEventListener("DOMcontentloaded", () => {
-const cardsAdj = [ 
-  {
-    name:'Caballo',
-    img:'imagenes/Caballo.jpg'
-  },
-  
+  const cardsAdj = [
+    {
+      name: "Caballo",
+      img: "imagenes/Caballo.jpg"
+    },
 
-{
-  name:'Cabra',
-  img:'imagenes/Cabra.jpg'
-},
+    {
+      name: "Cabra",
+      img: "imagenes/Cabra.jpg"
+    },
 
-{
-  name:'Cerdo',
-  img:'imagenes/Cerdo.jpg'
-},
+    {
+      name: "Cerdo",
+      img: "imagenes/Cerdo.jpg"
+    },
 
+    {
+      name: "Conejo",
+      img: "imagenes/Conejo.jpg"
+    },
 
+    {
+      name: "Oveja",
+      img: "imagenes/Oveja.jpg"
+    },
 
-{
-  name:'Conejo',
-  img:'imagenes/Conejo.jpg'
-},
+    {
+      name: "Pavo",
+      img: "imagenes/Pavo.jpg"
+    },
 
-{
-  name:'Oveja',
-  img:'imagenes/Oveja.jpg'
-},
+    {
+      name: "Caballo",
+      img: "imagenes/Caballo.jpg"
+    },
 
-{
-  name:'Pavo',
-  img:'imagenes/Pavo.jpg'
-},
+    {
+      name: "Cabra",
+      img: "imagenes/Cabra.jpg"
+    },
+
+    {
+      name: "Cerdo",
+      img: "imagenes/Cerdo.jpg"
+    },
+
+    {
+      name: "Conejo",
+      img: "imagenes/Conejo.jpg"
+    },
+
+    {
+      name: "Oveja",
+      img: "imagenes/Oveja.jpg"
+    },
+
+    {
+      name: "Pavo",
+      img: "imagenes/Pavo.jpg"
+    }
+  ];
+});
